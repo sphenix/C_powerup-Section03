c3bb1dc94ba928af37b0935e9619554dffc6c54d
sphenix <sphenixh2@gmail.com>
2015-01-09 17:11:16 +0900

    [STEP01] step03 solution

---
+++ sol_Project_step03/cusManager.c
@@ -0,0 +1,75 @@
+/* Name : cusManager	ver 1.2
+ * Content : 고객 관련 업무 처리 함수들의 정의
+ * Implementation : YSW
+ * 
+ * Last modified 2015/01/05
+ */
+#include "common.h"
+#include "cusInfo.h"
+#include "cusInfoAccess.h"
+#include "screenOut.h"
+
+/* 함 수 : void RegistCustomer(void)
+ * 기 능 : 신규 회원 가입
+ * 반 환 : void
+ *
+ */
+void RegistCustomer(void)
+{
+	char ID[ID_LEN];
+	char name[NAME_LEN];
+	char phoneNum[PHONE_LEN];
+
+	fputs("ID 입력 : ", stdout);
+	gets(ID);
+
+	if (IsRegistID(ID))
+	{
+		puts("해당 ID는 사용 중에 있습니다. 다른 ID를 선택해 주세요");
+		getchar();
+		return;
+	}
+	
+	fputs("이름 입력 : ", stdout);
+	gets(name);
+
+	fputs("전화번호 입력 : ", stdout);
+	gets(phoneNum);
+	
+	if (!AddCusInfo(ID, name, phoneNum)) 
+	{
+		puts("정상적인 데이터 저장에 실패하였습니다.");
+		getchar();
+		return;
+	}
+
+	puts("가입이 완료되었습니다.");
+	getchar();
+}
+
+/* 함 수 : void SearchCusInfo(void)
+ * 기 능 : ID를 통한 회원 정보 검색
+ * 반 환 : void
+ *
+ */
+void SearchCusInfo(void)
+{
+	char ID[ID_LEN];
+	cusInfo *cusPtr;
+
+	fputs("찾을 ID 입력 : ", stdout);
+	gets(ID);
+
+	cusPtr = GetCusPtrByID(ID);
+	if (cusPtr == 0)
+	{
+		puts("존재하지 않는 ID입니다.");
+		getchar();
+		return;
+	}
+
+	ShowCustomerInfo(cusPtr);
+}
+
+/* end of file */
+
---
+++ sol_Project_step03/screenOut.h
@@ -0,0 +1,21 @@
+/* Name : screenOut.h	ver 1.2
+ * Content : 콘솔 출력을 위한 함수들의 선언
+ * Implementation : YSW
+ *
+ * Last modified 2015/01/09
+ */
+
+#ifndef __SCREENOUT_H__
+#define __SCREENOUT_H__
+
+#include "cusInfo.h"
+#include "dvdInfo.h"
+
+void ShowMenu(void);
+void ShowCustomerInfo(cusInfo *pCus);
+void ShowCustomerInfoContinue(cusInfo *pCus);
+void ShowDVDInfo(dvdInfo *pDVD);
+void ShowGenre(int genre);
+
+#endif
+/* end of file */
